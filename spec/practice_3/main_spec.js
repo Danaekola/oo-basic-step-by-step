7d53b10959c356d5d0fb7f553497208f05df7404
Danaekola <danaekola@outlook.com>
2015-08-28 11:01:38 +0800

    finished practice_3

--- spec/practice_3/main_spec.js
+++ spec/practice_3/main_spec.js
@@ -3,11 +3,46 @@ var _ = require("lodash");
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
+var Person = require('../../src/practice_3.js');
+var Student = require('../../src/practice_3.js');
 var expect = chai.expect;
 chai.use(sinonChai);
 
-describe("OO入门", function(){
-    it("覆盖父类的方法-1", function(){
-    
+describe("jicehng", function() {
+    var person = new Person("Bob", 21);
+    var student = new Student(2);
+    describe("Person", function () {
+        it("#introduce", function () {
+
+            it('have name and age', function () {
+                //var person = new Person("Bob", 21);
+                expect(person.age).equal(21);
+                expect(person.name).equal('Bob');
+            });
+            it('can return a "string"', function () {
+                //var person = new Person("Bob", 21);
+                var result = person.introduce();
+                expect(result).equal('My name is Bob. I am 21 years old.');
+
+            });
+        });
+    });
+    describe("Student", function () {
+        it("#introduce", function () {
+
+            it('have name and age and class', function () {
+
+                //var student = new Student("Bob", 21,2);
+                expect(student.age).equal(21);
+                expect(student.name).equal('Bob');
+                expect(student.klass).equals(2);
+            });
+            it('can return a "string"', function () {
+                //var student = new Student("Bob", 21,2);
+                var result = student.introduce();
+                expect(result).equal('I am a Student. I am at Class 2.');
+
+            });
+        });
     });
 });
