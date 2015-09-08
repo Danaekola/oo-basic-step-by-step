1e5adc64b4a1d885c5af4491e01516950008315f
Danaekola <danaekola@outlook.com>
2015-09-08 16:45:21 +0800

    finished practice_3

--- spec/practice_3/main_spec.js
+++ spec/practice_3/main_spec.js
@@ -3,46 +3,44 @@ var _ = require("lodash");
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
-var Person = require('../../src/practice_3.js');
-var Student = require('../../src/practice_3.js');
 var expect = chai.expect;
 chai.use(sinonChai);
+var Person = require("../../src/practice_3/person.js");
+var Student = require("../../src/practice_3/student.js");
 
-describe("jicehng", function() {
-    var person = new Person("Bob", 21);
-    var student = new Student(2);
-    describe("Person", function () {
-        it("#introduce", function () {
+describe("Person", function(){
+    it("should have field name and age", function(){
+        var person = new Person("Tom", 21);
+        expect(person.name).to.equal("Tom");
+        expect(person.age).to.equal(21);
 
-            it('have name and age', function () {
-                //var person = new Person("Bob", 21);
-                expect(person.age).equal(21);
-                expect(person.name).equal('Bob');
-            });
-            it('can return a "string"', function () {
-                //var person = new Person("Bob", 21);
-                var result = person.introduce();
-                expect(result).equal('My name is Bob. I am 21 years old.');
-
-            });
-        });
     });
-    describe("Student", function () {
-        it("#introduce", function () {
 
-            it('have name and age and class', function () {
+    it("should have a method introduce, introduce person with name and age", function(){
+        var person = new Person("Tom", 21);
 
-                //var student = new Student("Bob", 21,2);
-                expect(student.age).equal(21);
-                expect(student.name).equal('Bob');
-                expect(student.klass).equals(2);
-            });
-            it('can return a "string"', function () {
-                //var student = new Student("Bob", 21,2);
-                var result = student.introduce();
-                expect(result).equal('I am a Student. I am at Class 2.');
+        var introduce = person.introduce();
 
-            });
-        });
+        expect(introduce).to.equal("My name is Tom. I am 21 years old.");
+
     });
+
+    describe("Student", function(){
+        it("should have field name, age and class number", function(){
+            var student = new Student("Tom", 21, 2);
+            expect(student.name).to.equal("Tom");
+            expect(student.age).to.equal(21);
+            expect(student.klass).to.equal(2);
+
+        });
+
+        it("should overwrite Person introduce, introduce student with class", function(){
+            var student = new Student("Tom", 21, 2);
+            var introduce = student.introduce();
+
+            expect(introduce).to.equal("I am a Student. I am at Class 2.");
+
+        });
+
+    })
 });
